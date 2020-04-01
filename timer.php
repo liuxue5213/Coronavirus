1a146a0a8a4523c41dc2efe32b6c809db74c410b
liuxue5213 <956149307@qq.com>
2020-04-01 14:47:54 +0800

    udpate timer

--- timer.php
+++ timer.php
@@ -1,20 +1,20 @@
 <?php
-require_once '../data/get.php';
-require_once '../data/corona.php';
-require_once '../data/other.php';
+require_once 'data/get.php';
+require_once 'data/corona.php';
+require_once 'data/other.php';
 require_once 'baidu/get.php';
 
-// $corona = new Corona();
-// $corona->index();
-// var_dump('data refresh success');
+$corona = new Corona();
+$corona->index();
+var_dump('data refresh success');
 
-// $ci = new CoronaInfo();
-// $ci->index();
-// var_dump('info refresh success');
+$ci = new CoronaInfo();
+$ci->index();
+var_dump('info refresh success');
 
-// $oc = new OtherCountry();
-// $data = $oc->index();
-// var_dump('info refresh success');
+$oc = new OtherCountry();
+$data = $oc->index();
+var_dump('info refresh success');
 
 $baidu = new Baidu();
 $data = $oc->baidu();
