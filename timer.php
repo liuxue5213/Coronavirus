5a375b34191ceac7d699ff694665c90f47d6abd8
liuxue5213 <956149307@qq.com>
2020-04-02 23:31:02 +0800

    update timer

--- timer.php
+++ timer.php
@@ -4,17 +4,17 @@ require_once 'data/corona.php';
 require_once 'data/other.php';
 require_once 'baidu/get.php';
 
-// $corona = new Corona();
-// $corona->index();
-// var_dump('data refresh success');
+$corona = new Corona();
+$corona->index();
+var_dump('data refresh success');
 
-// $ci = new CoronaInfo();
-// $ci->index();
-// var_dump('info refresh success');
+$ci = new CoronaInfo();
+$ci->index();
+var_dump('info refresh success');
 
-// $oc = new OtherCountry();
-// $data = $oc->index();
-// var_dump('info refresh success');
+$oc = new OtherCountry();
+$data = $oc->index();
+var_dump('info refresh success');
 
 $baidu = new Baidu();
 $data = $baidu->index();
