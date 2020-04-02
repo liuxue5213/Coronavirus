5bc5a5a3efe1404377812e521a6f68cfc8eabc56
liuxue5213 <956149307@qq.com>
2020-04-02 23:39:15 +0800

    update js

--- baidu.php
+++ baidu.php
@@ -242,10 +242,8 @@ $frtData = isset($data['foreign_realtime_data']) ? $data['foreign_realtime_data'
         </div>
 
         <script>
-            $(function() {
-
-
-            });
+            // $(function() {
+            // });
 
             function showRt() {
                 $('.hideRt').show();
@@ -256,7 +254,7 @@ $frtData = isset($data['foreign_realtime_data']) ? $data['foreign_realtime_data'
                 $('#hideFrt').hide();
             }
         </script>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="js/jquery-3.3.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
